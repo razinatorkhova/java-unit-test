3647d80db4a169457f34e2b9ce1e22e81227b3f6
Razina Torkhova <razina.torkhova@gmail.com>
2024-08-08 14:14:36 +0400

    My first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+Инициализируй Git в папке yandex.
+С помощью консоли cоздай в папке yandex два файла: Main.java и App.java.
+Запиши в файл Main.java простую программу:
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Привет, Git!);
+    }
+
+}
